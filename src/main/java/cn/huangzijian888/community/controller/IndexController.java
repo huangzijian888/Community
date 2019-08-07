c191887e42ee9fc3ec39757d4d3953975c02ad13
huangzijian888 <huangzijian888@gmail.com>
2019-08-07 22:03:30 +0800

    增加检查cookie

--- src/main/java/cn/huangzijian888/community/controller/IndexController.java
+++ src/main/java/cn/huangzijian888/community/controller/IndexController.java
@@ -1,15 +1,36 @@
 package cn.huangzijian888.community.controller;
 
+import cn.huangzijian888.community.mapper.UserMapper;
+import cn.huangzijian888.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author huangzijian888
  */
 @Controller
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 
--- src/main/java/cn/huangzijian888/community/mapper/UserMapper.java
+++ src/main/java/cn/huangzijian888/community/mapper/UserMapper.java
@@ -3,6 +3,7 @@ package cn.huangzijian888.community.mapper;
 import cn.huangzijian888.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author huangzijian888
@@ -11,9 +12,19 @@ import org.apache.ibatis.annotations.Mapper;
 public interface UserMapper {
 
     /**
-     * @param user
      * 插入用户信息
+     *
+     * @param user
      */
     @Insert("insert into USER  (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    /**
+     * 通过token查找用户
+     *
+     * @param token
+     * @return
+     */
+    @Select("select * from user where token = #{token}")
+    User findByToken(String token);
 }
