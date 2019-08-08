0405aba37ce6a32bb2811e5e502665631ce3e143
huangzijian888 <huangzijian888@gmail.com>
2019-08-08 10:12:55 +0800

    添加lombok支持

--- src/main/java/cn/huangzijian888/community/dto/GithubUser.java
+++ src/main/java/cn/huangzijian888/community/dto/GithubUser.java
@@ -1,43 +1,13 @@
 package cn.huangzijian888.community.dto;
 
+import lombok.Data;
+
 /**
  * @author huangzijian888
  */
+@Data
 public class GithubUser {
     private String name;
     private Long id;
     private String bio;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
-
-    @Override
-    public String toString() {
-        return "GithubUser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bio='" + bio + '\'' +
-                '}';
-    }
 }
